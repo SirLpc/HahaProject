0f43f9b7c84ba41f78427b078f02af90ddc04a67
sirlpc <sirlpc123@gmail.com>
2016-10-06 12:59:09 +0800

    no message

---
+++ Assets/TiltTestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TiltTestScript : MonoBehaviour
+{
+    float msg = 0f;
+
+	void Update ()
+    {
+        msg = CrossPlatformInputManager.GetAxis("Mouse X");
+        
+	}
+
+    void OnGUI()
+    {
+        GUILayout.Label("                     InputMousePos : " + msg.ToString());
+    }
+
+}
---
+++ Assets/_Scripts/Utils/DebugManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugManager : SA_Singleton<DebugManager>
+{
+
+    private string _msg = string.Empty;
+
+    public void Log(string msg)
+    {
+        _msg += ("\t" + msg);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(_msg);
+    }
+
+}
